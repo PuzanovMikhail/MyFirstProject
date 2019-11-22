15f8ef48781fd25319bed92288865cf9edb6e376
Puzanov Mikhail <55325335+PuzanovMikhail@users.noreply.github.com>
2019-11-22 16:16:43 +0200

    The basic implementation of the -header- block is made

---
+++ desktop.blocks/header/header.bemhtml.js
@@ -0,0 +1,12 @@
+block('header')(
+  content()(function(){
+
+      return [
+        {
+          elem: 'text',
+          content: 'content header in block header'
+        }
+      ]
+    
+  })
+)
